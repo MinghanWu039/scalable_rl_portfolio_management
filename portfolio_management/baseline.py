2643cf40e09da71f506d6c919d88f6f2a736e6ec
Minghan <miw039@ucsd.edu>
2025-06-06 20:58:51 -0700

    Singled out train and test functions

--- portfolio_management/baseline.py
+++ portfolio_management/baseline.py
@@ -93,7 +93,94 @@ def backtesting(env_test, model):
 
     merged = model_stats.merge(baseline_stats, left_index=True, right_index=True, suffixes=('_model', '_baseline'))
     merged.to_csv(f'backtests/final/backtest_{short_name_sha256("_".join(tics))}_{now}.csv')
-    # pd.DataFrame({'dates': dates, 'weights': weight_history}).to_csv(f'weights/' + short_name_sha256('_'.join(tics)) + total_date_range + ".csv")
+    pd.DataFrame({'dates': dates, 'weights': weight_history}).to_csv(f'weights/' + short_name_sha256('_'.join(tics)) + total_date_range + ".csv")
+
+def train(config, model_path, data_path, model_name,
+          algo='sac', retrain=False, features=['close', 'high', 'low'], device='cpu'):
+    raw_df = pd.read_csv(data_path)
+    raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]]
+
+    print('PROCESSING DATA')
+    df = preprocess(raw_df, config['train_start_date'], config['train_end_date'])
+    assert np.all(np.isfinite(df[["date", "tic", "close", "high", "low", "volume"]]).values), "Dataframe contains non-finite values (NaN, Inf, or -Inf)"
+
+    environment = PortfolioOptimizationEnv(
+        df,
+        initial_amount=config['initial_amount'],
+        comission_fee_pct=0.0025,
+        time_window=50,
+        reward_scaling=float(config['reward_scaling']),
+        features=features)
+    
+    sb_env, _ = environment.get_sb_env()
+    agent = DRLAgent(env=sb_env)
+    model_params = config.get("model", {})
+    model_params['device'] = device
+    if retrain:
+        assert model_path is not None, "Model path must be provided for retraining"
+        if algo == "ppo":
+            from stable_baselines3 import PPO as model_class
+        elif algo == "sac":
+            from stable_baselines3 import SAC as model_class
+        elif algo == "a2c":
+            from stable_baselines3 import A2C as model_class
+        elif algo == "ddpg":
+            from stable_baselines3 import DDPG as model_class
+        elif algo == "td3":
+            from stable_baselines3 import TD3 as model_class
+        else:
+            raise ValueError(f"Unsupported algorithm: {algo}")
+        model = model_class.load(os.path.join(model_path, model_name + ".zip"), env=sb_env, **model_params)
+    else:
+        model = agent.get_model(algo, model_kwargs=model_params)
+
+    # set up logger
+    now = datetime.now().strftime('%Y%m%d-%Hh%M')
+    tmp_path = f'log{now}/{algo}_{model_name}'
+    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # Set new logger
+    model.set_logger(new_logger)
+
+    # Train the model
+    checkpoint_dir = f"checkpoints/{now}/{algo}_{model_name}"
+    checkpoint_callback = CheckpointCallback(save_freq=20000, save_path=checkpoint_dir, name_prefix=f"{algo}_model")
+    print('BEGIN TRAINING')
+    trained_model = agent.train_model(model=model, tb_log_name=algo, total_timesteps=config['timesteps'], callback=checkpoint_callback)
+
+    # Save the model
+    trained_model.save(f"{model_path}/{model_name}.zip")
+    print(f"Model saved to {model_path}")
+
+def test(config, model_path, data_path, model_name,
+         algo='sac', retrain=False, features=['close', 'high', 'low'], device='cpu'):
+    if algo == "ppo":
+        from stable_baselines3 import PPO as model_class
+    elif algo == "sac":
+        from stable_baselines3 import SAC as model_class
+    elif algo == "a2c":
+        from stable_baselines3 import A2C as model_class
+    elif algo == "ddpg":
+        from stable_baselines3 import DDPG as model_class
+    elif algo == "td3":
+        from stable_baselines3 import TD3 as model_class
+    else:
+        raise ValueError(f"Unsupported algorithm: {algo}")
+    
+    trained_model = model_class.load(os.path.join(model_path, model_name + ".zip"), device=device)
+    raw_df = pd.read_csv(data_path)
+    raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]].drop_duplicates()
+
+    print('PROCESSING DATA')
+    df = preprocess(raw_df, config['test_start_date'], config['test_end_date'])
+    environment = PortfolioOptimizationEnv(
+        df,
+        initial_amount=config['initial_amount'],
+        comission_fee_pct=0.0025,
+        time_window=50,
+        reward_scaling=float(config['reward_scaling']),
+        features=features) # ,'close_30_sma', 'close_60_sma', 'volume'
+    backtesting(environment, trained_model)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='FinRL Baseline')
@@ -123,120 +210,122 @@ if __name__ == "__main__":
     total_date_range = "_2009-01-01_2024-10-01"
 
     if args.test:
-        if args.algo == "ppo":
-            from stable_baselines3 import PPO as model_class
-        elif args.algo == "sac":
-            from stable_baselines3 import SAC as model_class
-        elif args.algo == "a2c":
-            from stable_baselines3 import A2C as model_class
-        elif args.algo == "ddpg":
-            from stable_baselines3 import DDPG as model_class
-        elif args.algo == "td3":
-            from stable_baselines3 import TD3 as model_class
-        else:
-            raise ValueError(f"Unsupported algorithm: {args.algo}")
-        if args.sha256 and not (".zip" in args.model_path):
-            model_name = short_name_sha256('_'.join(tics))
-            trained_model = model_class.load(os.path.join(args.model_path, model_name + ".zip"))
-        else:
-            trained_model = model_class.load(args.model_path)
-        if args.data_path:
-            if args.sha256 and not (".csv" in args.data_path):
-                raw_df = pd.read_csv(os.path.join(args.data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
-            else:
-                raw_df = pd.read_csv(args.data_path)
-        else:
-            data_path = "data/sub"
-            if args.sha256:
-                raw_df = pd.read_csv(os.path.join(data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
-            else:
-                raw_df = pd.read_csv(data_path)
-        raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]].drop_duplicates()
-        print('PROCESSING DATA')
-        df = preprocess(raw_df, config['test_start_date'], config['test_end_date'])
-        environment = PortfolioOptimizationEnv(
-            df,
-            initial_amount=config['initial_amount'],
-            comission_fee_pct=0.0025,
-            time_window=50,
-            reward_scaling=float(config['reward_scaling']),
-            features=features) # ,'close_30_sma', 'close_60_sma', 'volume'
-        backtesting(environment, trained_model)
+        pass
+        # if args.algo == "ppo":
+        #     from stable_baselines3 import PPO as model_class
+        # elif args.algo == "sac":
+        #     from stable_baselines3 import SAC as model_class
+        # elif args.algo == "a2c":
+        #     from stable_baselines3 import A2C as model_class
+        # elif args.algo == "ddpg":
+        #     from stable_baselines3 import DDPG as model_class
+        # elif args.algo == "td3":
+        #     from stable_baselines3 import TD3 as model_class
+        # else:
+        #     raise ValueError(f"Unsupported algorithm: {args.algo}")
+        # if args.sha256 and not (".zip" in args.model_path):
+        #     model_name = short_name_sha256('_'.join(tics))
+        #     trained_model = model_class.load(os.path.join(args.model_path, model_name + ".zip"))
+        # else:
+        #     trained_model = model_class.load(args.model_path)
+        # if args.data_path:
+        #     if args.sha256 and not (".csv" in args.data_path):
+        #         raw_df = pd.read_csv(os.path.join(args.data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
+        #     else:
+        #         raw_df = pd.read_csv(args.data_path)
+        # else:
+        #     data_path = "data/sub"
+        #     if args.sha256:
+        #         raw_df = pd.read_csv(os.path.join(data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
+        #     else:
+        #         raw_df = pd.read_csv(data_path)
+        # raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]].drop_duplicates()
+        # print('PROCESSING DATA')
+        # df = preprocess(raw_df, config['test_start_date'], config['test_end_date'])
+        # environment = PortfolioOptimizationEnv(
+        #     df,
+        #     initial_amount=config['initial_amount'],
+        #     comission_fee_pct=0.0025,
+        #     time_window=50,
+        #     reward_scaling=float(config['reward_scaling']),
+        #     features=features) # ,'close_30_sma', 'close_60_sma', 'volume'
+        # backtesting(environment, trained_model)
         
     else:
-        if args.data_path:
-            if args.sha256 and not (".csv" in args.data_path):
-                raw_df = pd.read_csv(os.path.join(args.data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
-            else:
-                raw_df = pd.read_csv(args.data_path)
-        else:
-            data_path = "data/sub"
-            if args.sha256:
-                raw_df = pd.read_csv(os.path.join(data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
-            else:
-                raise ValueError("Data path must be provided for training")
+        pass
+        # if args.data_path:
+        #     if args.sha256 and not (".csv" in args.data_path):
+        #         raw_df = pd.read_csv(os.path.join(args.data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
+        #     else:
+        #         raw_df = pd.read_csv(args.data_path)
+        # else:
+        #     data_path = "data/sub"
+        #     if args.sha256:
+        #         raw_df = pd.read_csv(os.path.join(data_path, short_name_sha256('_'.join(tics)) + total_date_range + ".csv"))
+        #     else:
+        #         raise ValueError("Data path must be provided for training")
             
-        raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]]
+        # raw_df = raw_df[["date", "tic", "close", "high", "low", "volume"]]
 
-        print('PROCESSING DATA')
-        df = preprocess(raw_df, config['train_start_date'], config['train_end_date'])
-        # 'close', 'high', 'low', 'macd', 'boll_ub', 'boll_lb', 'rsi_30', 'cci_30', 'dx_30', 'close_30_sma', 'close_60_sma', 'vix'
-        assert np.all(np.isfinite(df[features]).values), "Dataframe contains non-finite values (NaN, Inf, or -Inf)"
+        # print('PROCESSING DATA')
+        # df = preprocess(raw_df, config['train_start_date'], config['train_end_date'])
+        # # 'close', 'high', 'low', 'macd', 'boll_ub', 'boll_lb', 'rsi_30', 'cci_30', 'dx_30', 'close_30_sma', 'close_60_sma', 'vix'
+        # assert np.all(np.isfinite(df[features]).values), "Dataframe contains non-finite values (NaN, Inf, or -Inf)"
 
-        environment = PortfolioOptimizationEnv(
-            df,
-            initial_amount=config['initial_amount'],
-            comission_fee_pct=0.0025,
-            time_window=50,
-            reward_scaling=float(config['reward_scaling']),
-            features=features)
+        # environment = PortfolioOptimizationEnv(
+        #     df,
+        #     initial_amount=config['initial_amount'],
+        #     comission_fee_pct=0.0025,
+        #     time_window=50,
+        #     reward_scaling=float(config['reward_scaling']),
+        #     features=features)
         
-        algo = args.algo
-        sb_env, _ = environment.get_sb_env()
-        agent = DRLAgent(env=sb_env)
-        model_params = config.get("model", {})
-        model_params['device'] = device
-        if args.retrain:
-            assert args.model_path is not None, "Model path must be provided for retraining"
-            if args.algo == "ppo":
-                from stable_baselines3 import PPO as model_class
-            elif args.algo == "sac":
-                from stable_baselines3 import SAC as model_class
-            elif args.algo == "a2c":
-                from stable_baselines3 import A2C as model_class
-            elif args.algo == "ddpg":
-                from stable_baselines3 import DDPG as model_class
-            elif args.algo == "td3":
-                from stable_baselines3 import TD3 as model_class
-            else:
-                raise ValueError(f"Unsupported algorithm: {args.algo}")
-            if args.sha256 and not (".zip" in args.model_path):
-                model_name = short_name_sha256('_'.join(tics))
-                model = model_class.load(os.path.join(args.model_path, model_name + ".zip"), env=sb_env, **model_params)
-            else:
-                model = model_class.load(args.model_path, env=sb_env, **model_params)
-        else:
-            model = agent.get_model(algo, model_kwargs=model_params)
+        # algo = args.algo
+        # sb_env, _ = environment.get_sb_env()
+        # agent = DRLAgent(env=sb_env)
+        # model_params = config.get("model", {})
+        # model_params['device'] = device
+        # if args.retrain:
+        #     assert args.model_path is not None, "Model path must be provided for retraining"
+        #     if args.algo == "ppo":
+        #         from stable_baselines3 import PPO as model_class
+        #     elif args.algo == "sac":
+        #         from stable_baselines3 import SAC as model_class
+        #     elif args.algo == "a2c":
+        #         from stable_baselines3 import A2C as model_class
+        #     elif args.algo == "ddpg":
+        #         from stable_baselines3 import DDPG as model_class
+        #     elif args.algo == "td3":
+        #         from stable_baselines3 import TD3 as model_class
+        #     else:
+        #         raise ValueError(f"Unsupported algorithm: {args.algo}")
+        #     if args.sha256 and not (".zip" in args.model_path):
+        #         model_name = short_name_sha256('_'.join(tics))
+        #         model = model_class.load(os.path.join(args.model_path, model_name + ".zip"), env=sb_env, **model_params)
+        #     else:
+        #         model = model_class.load(args.model_path, env=sb_env, **model_params)
+        # else:
+        #     model = agent.get_model(algo, model_kwargs=model_params)
 
-        # set up logger
-        now = datetime.now().strftime('%Y%m%d-%Hh%M')
-        tmp_path = f'log/{algo}/{now}'
-        new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-        # Set new logger
-        model.set_logger(new_logger)
+        # # set up logger
+        # now = datetime.now().strftime('%Y%m%d-%Hh%M')
+        # tmp_path = f'log/{algo}/{now}'
+        # new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+        # # Set new logger
+        # model.set_logger(new_logger)
 
-        # Train the model
-        checkpoint_dir = f"checkpoints/{algo}/{now}"
-        checkpoint_callback = CheckpointCallback(save_freq=20000, save_path=checkpoint_dir, name_prefix=f"{algo}_model")
-        print('BEGIN TRAINING')
-        trained_model = agent.train_model(model=model, tb_log_name=algo, total_timesteps=config['timesteps'], callback=checkpoint_callback)
+        # # Train the model
+        # checkpoint_dir = f"checkpoints/{algo}/{now}"
+        # checkpoint_callback = CheckpointCallback(save_freq=20000, save_path=checkpoint_dir, name_prefix=f"{algo}_model")
+        # print('BEGIN TRAINING')
+        # trained_model = agent.train_model(model=model, tb_log_name=algo, total_timesteps=config['timesteps'], callback=checkpoint_callback)
 
-        # Save the model
-        if args.model_path is not None:
-            trained_model.save(f"{args.model_path}/{short_name_sha256('_'.join(tics))}.zip")
-            # trained_model.save(args.model_path)
-            print(f"Model saved to {args.model_path}")
-        else:
-            trained_model.save(f"trained_models/{algo}/{now}.zip")
-            print(f"Model saved to models/{algo}/{now}")
+        # # Save the model
+        # if args.model_path is not None:
+        #     trained_model.save(f"{args.model_path}/{short_name_sha256('_'.join(tics))}.zip")
+        #     # trained_model.save(args.model_path)
+        #     print(f"Model saved to {args.model_path}")
+        # else:
+        #     trained_model.save(f"trained_models/{algo}/{now}.zip")
+        #     print(f"Model saved to models/{algo}/{now}")
     
