bac0ff35a8800b6c376d86756d4d5381b46823c1
Minghan <miw039@ucsd.edu>
2025-06-07 22:28:12 -0700

    Modified example.ipynb

--- riling/tics/downloader/download_tic.py
+++
@@ -1,7 +0,0 @@
-import bs4
-
-def download_tic(file_name, class_):
-    with open(file_name) as f:
-        html = f.read()
-    soup = bs4.BeautifulSoup(html)
-    return [tic.text.strip() for tic in soup.find_all('span', class_=class_)]
